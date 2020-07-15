b9339e1826356c1aad72c47bf8144db2ce556c49
caihongyu0226 <823696721@qq.com>
2020-07-15 09:21:22 +0800

    netdisk-user-api

---
+++ netdisk-user-api/src/main/java/com/micro/disk/user/bean/UserBean.java
@@ -0,0 +1,19 @@
+package com.micro.disk.user.bean;
+
+import java.io.Serializable;
+
+import lombok.Data;
+
+/**
+ * @author chy
+ * @version 1.0
+ * @date 2020/7/15 9:10
+ */
+
+@Data
+public class UserBean implements Serializable {
+    private String id;
+    private String nickname;
+    private String username;
+    private String telephone;
+}
